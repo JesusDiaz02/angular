436882f4612d91d5f3b363704b065cefe1ded04d
= <jdiaz89@cuc.edu.co>
2022-03-16 20:44:32 -0500

    commit sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { ViewEncapsulation } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
   title = 'my-contacts';
